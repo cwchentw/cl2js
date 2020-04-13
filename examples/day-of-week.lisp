abdef42ec37537add9d7a2385283c443a381966b
Michael Chen <cwchen123@gmail.com>
2020-04-13 23:02:29 +0800

    Update an example

--- examples/day-of-week.lisp
+++ examples/day-of-week.lisp
@@ -1,3 +1,17 @@
+; switch ((new Date()).getDay()) {
+; case 0:
+; case 6:
+;     console.log("Weekend");
+;     break;
+; case 5:
+;     console.log("Thank God. It's Friday!");
+;     break;
+; case 3:
+;     console.log("Hump day");
+;     break;
+; default:
+;     console.log("Week");
+; }
 (switch ((getprop (new (-Date)) 'get-day))
   (0 nil)
   (6 ((getprop console 'log) "Weekend")
