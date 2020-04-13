1c47f76733f919140e33ddffb4e9bbeb86031c01
Michael Chen <cwchen123@gmail.com>
2020-04-13 22:57:07 +0800

    Add an example

---
+++ examples/day-of-week.lisp
@@ -0,0 +1,10 @@
+(switch ((getprop (new (-Date)) 'get-day))
+  (0 nil)
+  (6 ((getprop console 'log) "Weekend")
+       break)
+  (5 ((getprop console 'log) "Thank God. It's Friday!")
+       break)
+  (3 ((getprop console 'log) "Hump day")
+       break)
+  (default
+     ((getprop console 'log) "Week")))
