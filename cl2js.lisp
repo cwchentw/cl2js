1b3d80a33faee9b7f1dd7ce94187c94cd14695d4
Michael Chen <cwchen.tw@outlook.com>
2020-04-30 05:45:10 +0800

    Refactor some code

--- cl2js.lisp
+++ cl2js.lisp
@@ -17,14 +17,8 @@
       (format t  "/* ~A */~%" form))
     (format t "~A~%" (ps:ps* form))))
 
-; Generate newer JavaScript code.
-(setq parenscript:*js-target-version* "1.8.5")
-
-(defun main ()
-  (prog* ((args (argument-vector))
-          #+(or sbcl ccl) (args (rest args))
-         )
-    (if (null (first args))
+(defun ps2js-read (in &optional verbose)
+  (if (null in)
         (with-open-stream (s *standard-input*)
           (if (not (interactive-stream-p s))
               (handler-bind
@@ -32,18 +26,27 @@
                    (lambda (e)
                      (format *error-output* "~A~%" e)
                      (quit-with-status 1))))
-                (ps2js s))
+                (ps2js s :comment verbose))
               (progn
                 (perror "No input file")
                 (quit-with-status 1))))
-        (with-open-file (f (first args))
+        (with-open-file (f in)
           (handler-bind
             ((error
                (lambda (e)
                  (format *error-output* "~A~%" e)
                  (quit-with-status 1))))
             ; We just disable verbose mode now.
-            (ps2js f)))))
+            (ps2js f :comment verbose)))))
+
+; Generate newer JavaScript code.
+(setq parenscript:*js-target-version* "1.8.5")
+
+(defun main ()
+  (prog* ((args (argument-vector))
+          #+(or sbcl ccl) (args (rest args))
+         )
+    (ps2js-read (first args)))
   (finish-output)
   (quit-with-status))
 
